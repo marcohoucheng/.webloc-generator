ce8f1e1f540bb9190ad837ac8dfe3b38091570db
Marco Lam <marcohoucheng@gmail.com>
2024-03-14 18:46:19 +0100

    Working script.

--- scraper/README.md
+++ scraper/README.md
@@ -5,3 +5,12 @@ The goal of this script is to create a method to download all reports available 
 The projects are submitted by students of the modules enrolled from 2015 to 2017. The reports can be found on [cs231n.stanford.edu/project.html](http://cs231n.stanford.edu/project.html).
 
 This script uses `BeautifulSoup4` and `selenium` libraries for Python.
+
+
+## Download web driver
+
+Chrome: https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+Firefox: https://github.com/mozilla/geckodriver/releases
+
+Safari:    https://webkit.org/blog/6900/webdriver-support-in-safari-10/ 
---
+++ scraper/scraper.py
@@ -0,0 +1,53 @@
+import requests, time, os
+from bs4 import BeautifulSoup 
+from selenium import webdriver
+
+years = ["2015", "2016", "2017"]
+base_url = "http://cs231n.stanford.edu/"
+
+# Initiate the webdriver.
+driver = webdriver.Safari('/usr/bin/safaridriver')
+# driver = webdriver.Chrome('./chromedriver')
+
+for year in years:
+    url = base_url + year + "/reports.html"
+
+    driver.get(url)
+    time.sleep(5)
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all("a", href=lambda x: x and x.endswith(".pdf"))
+    links = [link.get('href') for link in links if 'pdfs' in link.get('href')]
+
+    if year == '2017':
+        # Modify each link to include the full URL
+        links = [base_url + link.replace('../', '') for link in links]
+        links = list(set(links))
+
+    # Directory where you want to save the downloaded PDFs
+    download_directory = './scraper/Downloads/' + year + '/'
+    os.makedirs(download_directory, exist_ok = True)
+
+    # Download each PDF
+    for url in links:
+        # Get the file name by splitting the URL
+        file_name = url.split('/')[-1]
+        
+        # Full path to save the file
+        file_path = os.path.join(download_directory, file_name)
+        
+        # Make the GET request to download the PDF
+        response = requests.get(url)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Write the content to the file
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            print(f'Downloaded {url}')
+        else:
+            print(f'Failed to download {url}')
+
+# Closing the webdriver
+driver.close()
